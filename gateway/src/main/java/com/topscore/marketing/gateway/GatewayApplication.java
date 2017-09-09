1508cd83c2bb579921ced51f60d2bac1459cb427
dengb <dengbin19910916@163.com>
2017-09-09 09:32:18 +0800

    初始化框架

--- gateway/src/main/java/com/topscore/marketing/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/topscore/marketing/gateway/GatewayApplication.java
@@ -1,17 +1,16 @@
 package com.topscore.marketing.gateway;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-import static org.springframework.boot.WebApplicationType.SERVLET;
-
 @EnableZuulProxy
+@EnableEurekaClient
 @SpringBootApplication
 public class GatewayApplication {
 
 	public static void main(String[] args) {
-//		SpringApplication.run(GatewayApplication.class, args);
-		new SpringApplicationBuilder(GatewayApplication.class).web(SERVLET).run(args);
+		SpringApplication.run(GatewayApplication.class, args);
 	}
 }
--- ui/src/main/java/com/topscore/marketing/ui/SampleController.java
+++ ui/src/main/java/com/topscore/marketing/ui/SampleController.java
@@ -1,9 +1,11 @@
 package com.topscore.marketing.ui;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/sample")
 public class SampleController {
 
     @GetMapping("/hello")
